8deaa0580538f2593b01e3a00e5e26e3834c477f
Alan Bischoff <alanlbischoff@gmail.com>
2018-06-15 11:50:03 -0600

    working toggle

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import StarWars from './components/StarWars';
+import StarTrek from './components/StarTrek';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  state = { starwars: true };
+
+  toggleNerds = () => {
+    this.setState({ starwars: !this.state.starwars });
   }
+  render() {
+   if(this.state.starwars)
+   return(
+     <div>
+       <button onClick={ this.toggleNerds}>Toggle Star Trek</button>
+       <StarWars />
+       </div>
+   );
+   else
+   return (
+     <div>
+         <button onClick={ this.toggleNerds }>Toggle Star Wars</button>
+         <StarTrek />
+      </div>
+   );
+ }
 }
 
 export default App;
